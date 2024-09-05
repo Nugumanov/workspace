7e6bcb33f08c6ee0db2e0055ee95a09520eb73b3
Advena <advena@Advenas-Mac-mini.local>
2024-09-05 12:43:08 +0400

    Add checking if gruvbox theme is installed first

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,3 +1,21 @@
 -- lua/colors.lua
+
 vim.g.gruvbox_italic = 0
-vim.cmd('colorscheme gruvbox')
+
+-- Check if 'gruvbox' is installed, otherwise use a fallback
+local colorscheme = "gruvbox"
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+  -- If 'gruvbox' isn't available, use a default colorscheme as a fallback
+  vim.cmd [[colorscheme default]]
+end
+
+-- Automatically apply 'gruvbox' after PackerSync completes
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd User PackerComplete ++once lua vim.cmd("colorscheme gruvbox")
+  augroup end
+]]
+--TEST
