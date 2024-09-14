dc2ab97cdfa963cb1115986e215cce40e7d0e491
Advena <advena@Advenas-Mac-mini.local>
2024-09-14 16:15:44 +0400

    Add Mason setup for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,23 @@
 -- init.lua
+require('plugins')     -- Plugin manager
 require('settings')    -- General settings
 require('mappings')    -- Key mappings
 require('autocmds')    -- Autocommands
 require('colors')      -- Colors and UI settings
-require('plugins')     -- Plugin manager
 
 -- Load plugin-specific configurations
 require('plugin-config.fern')
 require('plugin-config.airline')
+
+-- Mason Setup
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+        },
+    }
+})
+require("mason-lspconfig").setup()
+
